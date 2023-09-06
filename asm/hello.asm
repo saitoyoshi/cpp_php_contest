eff338ff3bbc22158b8b94c585eef5283dd45dc2
saitoyoshi <mitti180@gmail.com>
2023-09-06 18:27:46 +0900

    20230906アセンブラ学習

---
+++ asm/hello.asm
@@ -0,0 +1,19 @@
+bits 64
+section .text
+global _start
+
+_start:
+  xor eax, eax
+  mov edx, eax
+  inc eax
+  mov dl, len
+  mov rsi, msg
+  syscall
+  xor edi, edi
+  mov eax, edi
+  mov al, 60
+  syscall
+
+section .data
+  msg db 'hello, world', 0x0A
+  len equ $ - msg
