db9d4871cd5e3b7bebe886b1c792c48bbcf5e532
saitoyoshi <mitti180@gmail.com>
2023-09-29 08:59:56 +0900

    20230929

---
+++ abcs/178/170/167/111/a.php
@@ -0,0 +1,22 @@
+<?php
+
+$N = (int) trim(fgets(STDIN));
+if ($N <= 111) {
+    echo 111 .PHP_EOL;
+} elseif ($N <= 222) {
+    echo 222 .PHP_EOL;
+} elseif ($N <= 333) {
+    echo 333 .PHP_EOL;
+} elseif ($N <= 444) {
+    echo 444 .PHP_EOL;
+} elseif ($N <= 555) {
+    echo 555 .PHP_EOL;
+} elseif ($N <= 666) {
+    echo 666 . PHP_EOL;
+} elseif ($N <= 777) {
+    echo 777 . PHP_EOL;
+} elseif ($N <= 888) {
+    echo 888 . PHP_EOL;
+} else {
+    echo 999 . PHP_EOL;
+}
