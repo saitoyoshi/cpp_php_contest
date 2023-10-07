3456fcce2e24512ce39075e4e51e5f9a2fa3c17d
saitoyoshi <mitti180@gmail.com>
2023-10-08 06:06:59 +0900

    20231008

--- lisp/land/sketch.lisp
+++ lisp/land/sketch.lisp
@@ -1,110 +1,18 @@
 (defparameter *small* 1)
 (defparameter *big* 100)
+
 (defun guess-my-number ()
   (ash (+ *small* *big*) -1))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-; (defparameter *small* 1)
-; (defparameter *big* 100)
-
-; (defun guess-my-number ()
-;   (ash (+ *small* *big*) -1))
-
-; (defun smaller ()
-;   (setf *big* (1- (guess-my-number)))
-;   (guess-my-number))
-
-
-; (defun bigger ()
-;   (setf *small* (1+ (guess-my-number)))
-;   (guess-my-number))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-; (defun smaller ()
-;   (setf *big* (1- (guess-my-number)))
-;   (guess-my-number))
-
-; (defun bigger ()
-;   (setf *small* (1+ (guess-my-number)))
-;   (guess-my-number))
-
-; ライブラリ、自身
-; 組み込み機能が豊富、言語自身をいじれる
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-; ((setf (*small* 1))
-; (setf (*big* 100))
-; (defparameter (*small* 1))
-; (defparameter (*big* 100))
-
-; (defparameter *small* 1)
-; (defparameter *big* 100)
-
-; (defun guess-my-number ()
-;   (ash (+ *small* *big*) -1))
-
-; (let ((a 5)
-;   (b 6))
-;   (+ a b))
-
-; (flet (f (n) (+ n 10)) (f 15))
-
-
-; ・組み込みの機能が豊富
-; ・言語自体をいじれる
-
-; ライブラリ、言語
+(defun smaller ()
+  (setf *big* (1- (guess-my-number)))
+  (guess-my-number))
+
+(defun bigger ()
+  (setf *small* (1+ (guess-my-number)))
+  (guess-my-number))
+
+(defun start-over ()
+  (defparameter *small* 1)
+  (defparameter *big* 100)
+  (guess-my-number))
