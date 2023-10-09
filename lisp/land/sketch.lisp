a9e729def7e8d7c94763d5b952983706780330ec
saitoyoshi <mitti180@gmail.com>
2023-10-09 09:09:45 +0900

    20231009復習

--- lisp/land/sketch.lisp
+++ lisp/land/sketch.lisp
@@ -1,29 +1,26 @@
-(princ "WE ARE")
 (defparameter *small* 1)
 (defparameter *big* 100)
 
 (defun guess-my-number ()
-  (ash (+ *small* *big*) -1))
-
-(defun smaller ()
-  (setf *big* (1- (guess-my-number)))
-  (guess-my-number))
+  (ash (+ *big* *small*) -1))
 
 (defun bigger ()
   (setf *small* (1+ (guess-my-number)))
   (guess-my-number))
 
-(defun start-over ()
-  (defparameter *small* 1)
-  (defparameter *big* 100)
+(defun smaller ()
+  (setf *big* (1- (guess-my-number)))
   (guess-my-number))
 
-
-
-
-
-
-
+; 1つ目のスロットにある値を取り出す
+; シングルキュートでエスケープ
+; コードモード、データモード
+; 他のコンスセル、それ以外のlispデータ
+; リストの先頭要素が特別なコマンドになっている
+; 最初の要素以外はすべて先頭のコマンドの引数になる
+; コンスセル
+; 2つの要素をつなげるコンスセルがただただ長く、連なったもの
+; 独立した単語
 ; 規則・意味
 ; リストで構成
 ; かっこを使ってリストを構成
@@ -58,3 +55,22 @@
 ; 2つの要素をつなげるコンスセルがただただながく、連なった
 ; データ、コード
 ; かっこをつかってコードをリストにする
+; (princ "WE ARE")
+; (defparameter *small* 1)
+; (defparameter *big* 100)
+
+; (defun guess-my-number ()
+;   (ash (+ *small* *big*) -1))
+
+; (defun smaller ()
+;   (setf *big* (1- (guess-my-number)))
+;   (guess-my-number))
+
+; (defun bigger ()
+;   (setf *small* (1+ (guess-my-number)))
+;   (guess-my-number))
+
+; (defun start-over ()
+;   (defparameter *small* 1)
+;   (defparameter *big* 100)
+;   (guess-my-number))
