086c6867de0fdfc7240728868fd10c40869c5b0e
saitoyoshi <mitti180@gmail.com>
2023-10-12 09:23:50 +0900

    20231012眼精疲労中

--- lisp/land/sketch.lisp
+++ lisp/land/sketch.lisp
@@ -1,9 +1,110 @@
-(defun check-number (n)
-  (case (signum n)
-    ((1) (format t "p"))
-    ((-1) (format t "m"))
-    ((0) (format t "z"))
-    (otherwise (format t "ho"))))
+; 1つ目のスロット
+; (defparameter *s* 1)
+; (defparameter *b* 100)
+; (defun gmn ()
+;   (ash (+ *s* *b*) -1))
+; (defun b ()
+;   (setf *s* (1+ (gmn)))
+;   (gmn))
+; (defun s ()
+;   (setf *b* (1- (gmn)))
+;   (gmn))
+; (defun so ()
+;   (defparameter *s* 1)
+;   (defparameter *b* 100)
+;   (gmn))
+
+; (defun s (s)
+;   (case s ('a 1)
+;           ('b 2)
+;           (t 3)))
+
+; (defun c (n)
+;   (case (signum n) ((1) '+)
+;           ((-1) '-)
+;           (otherwise 0)))
+;　2つの隣合うはこで、コンスセルや他のデータを指せる
+; データ、コード
+; 2つの要素をつなげるコンスセルがただただ長く連なったもの
+
+
+
+
+
+
+
+
+
+
+  ; (defun bigger ()
+  ; (setf *small* (1+ (guess-my-number)))
+  ; (guess-my-number))
+; (defun m ()
+;   ((setf (*small* (1+ (guess-my-number))))
+;   (guess-my-number)))
+; (defun bigger ()
+;   (setf *small* (1+ (guess-my-number)))
+;   (guess-my-number))
+; (defun cnc (n)
+;   (case (signum n)
+;   ((1) (format t "p"))
+;   ((-1) (format t "m"))
+;   ((0) (format t "z"))))
+
+; (defun check-n (n)
+;   (cond ((> n 0) (format t "p"))
+;   ((< n 0) (format t "m"))
+;   (t (format t "z"))))
+; (defparameter *big* 100)
+; (defparameter *small* 1)
+; (defun guess-my-number ()
+;   (ash (+ *big* *small*) -1))
+; (defun b ()
+;   (setf *small* (1+ (guess-my-number)))
+;   (guess-my-number))
+; (defun bigger ()
+;   (setf *small* (1+ (guess-my-number))) (guess-my-number))
+; カッコを使ってコードをリストにする
+; カッコをつかってリストをコード構成する
+
+
+; 豊かな組み込み機能、言語自身をいじれる
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+; (defun check-number (n)
+; (defun cnc (n)
+;   (case (signum n)
+;     ((1) (format t "p"))
+;     ((-1) (format t "m"))
+;     ((0) (format t "z"))))
+
+
+;   (case (signum n)
+;     ((1) (format t "p"))
+;     ((-1) (format t "m"))
+;     ((0) (format t "z"))
+;     (otherwise (format t "ho"))))
 ; (defun c-n-c (n)
 ;   (case n
 ;   ((> 0) (format t "se"))
