025c1497a484d0d965a7d15a3419d18ec05a4603
saitoyoshi <mitti180@gmail.com>
2023-10-20 08:53:36 +0900

    20231020

--- lisp/land/4.lisp
+++ lisp/land/4.lisp
@@ -1,3 +1,16 @@
 (let ((a 2) (b 3))
   (let ((c (+ 2 (* a b))))
     (+ c -1)))
+
+(let ((a 7))
+  (let ((a 2))
+    (+ a 2)))
+
+(let ((a 1))
+  (progn
+    (princ a)
+    (let ((a 2))
+      (progn
+        (princ a)
+        (let ((a 3))
+          (princ a))))))
